585148c665321e0c0a47d3e20d8a1813a70b00a0
vicabatin <batinvictoria@gmail.com>
2026-02-08 17:03:54 -0500

    integrated EKS module with root configuration

--- roots/devops-project-main/main.tf
+++ roots/devops-project-main/main.tf
@@ -24,3 +24,12 @@ module "vpc" {
   private_subnet_cidrs = var.private_subnet_cidrs
   cluster_name         = var.cluster_name
 }
+
+module "eks" {
+  source = "../../modules/eks"
+  
+  cluster_name = var.eks_cluster_name
+  vpc_id       = module.vpc.vpc_id
+  subnet_ids   = module.vpc.public_subnet_ids
+  env          = var.environment
+}
--- roots/devops-project-main/variables.tf
+++ roots/devops-project-main/variables.tf
@@ -36,3 +36,9 @@ variable "cluster_name" {
   description = "EKS cluster name"
   type        = string
 }
+
+variable "eks_cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
