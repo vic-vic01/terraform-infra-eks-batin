355024c812aa5142da12073a5ca09991c20eaedf
vicabatin <batinvictoria@gmail.com>
2026-02-16 21:50:37 -0500

    restore all access entries with policies and keep ebs-csi commented

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -200,13 +200,14 @@ resource "aws_eks_access_entry" "sso_admin" {
   principal_arn = var.sso_admin_arn
 }
 
+resource "aws_eks_access_entry" "github_ci" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = var.github_ci_role_arn
+}
+
 resource "aws_eks_access_entry" "github_terraform" {
   cluster_name  = aws_eks_cluster.main.name
   principal_arn = var.github_tf_role_arn
-  
-  lifecycle {
-    ignore_changes = all
-  }
 }
 
 resource "aws_eks_access_policy_association" "sso_admin_policy" {
@@ -219,6 +220,18 @@ resource "aws_eks_access_policy_association" "sso_admin_policy" {
   }
 
   depends_on = [aws_eks_access_entry.sso_admin]
+}
+
+resource "aws_eks_access_policy_association" "github_ci_policy" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = var.github_ci_role_arn
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+
+  depends_on = [aws_eks_access_entry.github_ci]
 }
 
 resource "aws_eks_access_policy_association" "github_terraform_policy" {
