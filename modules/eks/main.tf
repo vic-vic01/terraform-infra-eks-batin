01cee915189c47e00138d8da44512598efd1a6cd
vicabatin <batinvictoria@gmail.com>
2026-02-16 20:10:49 -0500

    remove ebs-csi addon to avoid timeout before node registration

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -225,15 +225,15 @@ resource "aws_eks_addon" "vpc_cni" {
   depends_on = [aws_autoscaling_group.eks_nodes]
 }
 
-resource "aws_eks_addon" "ebs_csi" {
-  cluster_name = aws_eks_cluster.main.name
-  addon_name   = "aws-ebs-csi-driver"
+# resource "aws_eks_addon" "ebs_csi" {
+#   cluster_name = aws_eks_cluster.main.name
+#   addon_name   = "aws-ebs-csi-driver"
 
-  timeouts {
-    create = "30m"
-    update = "30m"
-    delete = "15m"
-  }
+#   timeouts {
+#     create = "30m"
+#     update = "30m"
+#     delete = "15m"
+#   }
 
-  depends_on = [aws_autoscaling_group.eks_nodes]
-}
+#   depends_on = [aws_autoscaling_group.eks_nodes]
+# }
